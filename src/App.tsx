fa297b8778a550077882422ce29febc9855a68a8
yurii-serdiuk-gaming <yurii.serdiuk@authenticgaming.com>
2021-06-21 11:57:27 +0300

    added timer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
 import React from 'react';
+import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
+import {Button, Container} from "@material-ui/core";
+import Header from "./containers/header";
+import Count from "./containers/count";
 
 import './App.scss';
-import {Container} from "@material-ui/core";
-import Header from "./containers/header";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            height: '100vh',
+            textAlign: 'center',
+            padding: '0 !important',
+        },
+        main: {
+            display: 'flex',
+            justifyContent:'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            height: ' 92.2%',
+            backgroundColor: '#b3e5fc',
+        }
+    }),
+);
 
 function App() {
-  return (
-      <Container className='App' maxWidth={false}>
-        <Header/>
-    <main> Main</main>
-    </Container>
-  );
+    const classes = useStyles();
+
+    return (
+        <Container className={classes.root} maxWidth={false}>
+            <Header/>
+            <main className={classes.main}>
+                <Count/>
+                <Button variant="contained" color="primary">
+                    Start
+                </Button>
+            </main>
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/containers/count/Count.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const Count = () => {
+    return <div>00:00:00</div>
+}
+
